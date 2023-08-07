84727a044cb13131461283f3e09b497c0572e8de
Azhur <showipbgpall2@gmail.com>
2023-08-07 10:05:13 +0500

    Добавил MEDIA_URL в urlpatterns

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -3,12 +3,15 @@ from rest_framework.routers import DefaultRouter
 
 from api.views import (IngredientViewSet, RecipeViewSet, SubscriptionViewSet,
                        TagViewSet, UserSubscribeView)
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 router = DefaultRouter()
 router.register('recipes', RecipeViewSet, basename='recipes')
 router.register('ingredients', IngredientViewSet, basename='ingredients')
 router.register('tags', TagViewSet, basename='tags')
+
 
 urlpatterns = [
     path('users/subscriptions/', SubscriptionViewSet.as_view({'get': 'list'})),
@@ -17,3 +20,8 @@ urlpatterns = [
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
