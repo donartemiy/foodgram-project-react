6d6b1b5b2838ed75e3a4d5aa3e242ad975c2a47f
Azhur <showipbgpall2@gmail.com>
2023-08-08 21:25:31 +0500

    pep8

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,4 +1,3 @@
-# import django_filters as filters
 from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient, Recipe, Tag
@@ -9,25 +8,6 @@ class RecipeFilter(FilterSet):
         field_name="tags__slug",
         to_field_name="slug",
         queryset=Tag.objects.all(),)
-#     is_favorited = filters.CharFilter(method="get_is_favorited")
-#     is_in_shopping_cart = filters.CharFilter(
-#         method='get_is_in_shopping_cart')
-
-#     class Meta:
-#         model = Recipe
-#         fields = ["author", "tags", "is_favorited", "is_in_shopping_cart"]
-
-#     def get_is_favorited(self, queryset, name, value):
-#         user = self.request.user
-#         if value:
-#             return Recipe.objects.filter(favorite__user=user)
-#         return Recipe.objects.all()
-
-#     def get_is_in_shopping_cart(self, queryset, name, value):
-#         user = self.request.user
-#         if value:
-#             return Recipe.objects.filter(shopping_cart__user=user)
-#         return Recipe.objects.all()
     is_favorited = filters.BooleanFilter(
         method='get_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
