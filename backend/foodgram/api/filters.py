dafe5a72e7d420e10f84f752eba6bccb5752c3c7
Jura <31482424+donartemiy@users.noreply.github.com>
2023-08-08 13:34:50 +0500

    Update filters.py 2

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,5 +1,5 @@
 # import django_filters as filters
-from django_filters.rest_framework import FilterSet, filters
+from django_filters.rest_framework import filters
 
 from recipes.models import Ingredient, Recipe, Tag
 
