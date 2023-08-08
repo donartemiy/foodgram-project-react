f05168dee8888e533b26f675b112ddff3cd215c1
Jura <31482424+donartemiy@users.noreply.github.com>
2023-08-08 13:49:17 +0500

    Update filters.py 3

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -1,10 +1,10 @@
 # import django_filters as filters
-from django_filters.rest_framework import filters
+from django_filters.rest_framework import FilterSet, filters
 
 from recipes.models import Ingredient, Recipe, Tag
 
 
-class RecipeFilter(filters.FilterSet):
+class RecipeFilter(FilterSet):
     tags = filters.ModelMultipleChoiceFilter(
         field_name="tags__slug",
         to_field_name="slug",
@@ -30,7 +30,7 @@ class RecipeFilter(filters.FilterSet):
         return Recipe.objects.all()
 
 
-class IngredientFilter(filters.FilterSet):
+class IngredientFilter(FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="startswith")
 
     class Meta:
